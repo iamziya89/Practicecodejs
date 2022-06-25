f73af480c3736fa1793d9bfb663d174d04b3ca11
Mohd Ziya <iamziya53@gmail.com>
2022-06-24 21:00:24 -0400

    practice on function 24 junw

---
+++ anonymousfun.js
@@ -0,0 +1,10 @@
+let ans=(function (num){
+    let sum=0;
+    while(num!=0){
+        rem=num%10;
+        sum=sum+rem**2;
+        num=Math.floor(num/10);
+    }
+    return sum;
+})(26);
+console.log(ans)
