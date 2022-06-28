df458b07d950ba5d34191895e5f84c496957f8e7
Mohd Ziya <iamziya53@gmail.com>
2022-06-28 13:01:05 -0400

    Practice on call back function

---
+++ callBackfun2.js
@@ -0,0 +1,14 @@
+setTimeout(function (){
+    console.log("Hellow Worlds 1")
+},4000);
+setTimeout(function (){
+    console.log("Hellow Worlds 2")
+},5000);
+setTimeout(function (){
+    console.log("Hellow Worlds 3")
+},6000);
+
+function a(){
+    console.log("Hellow worlds")
+}
+a();
