cbabfbb0b1c7469b4febc6f8c5bda6866a90ea01
Mohd Ziya <iamziya53@gmail.com>
2022-06-28 15:24:09 -0400

    Practice

--- callBack.js
+++ callBack.js
@@ -1,4 +1,20 @@
 function a(){
-    console.log("Hellow World")
+    console.log("Hellow World 1")
 }
 setInterval(a,4000);
+function a(){
+    console.log("Hellow World 2")
+}
+setInterval(a,5000);
+function a(){
+    console.log("Hellow World 3")
+}
+setInterval(a,6000);
+function a(){
+    console.log("Hellow World")
+}
+a();
+function any(num1,num2){
+    console.log(num1+num2)
+}
+setTimeout(any,7000,6,8)
---
+++ session42/fun01.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+function x(){
+    console.log("Hello 2");
+}
+setTimeout(x,4000);
+console.log("Hello 3");
+console.log("Hello 4");
